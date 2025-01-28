6ff4246151fa8b1ae4e497e5d8f679cf140a9e62
Pranav-cse-kalvium <pranav.prakash@kalvium.community>
2025-01-28 12:04:03 +0530

    schema

---
+++ schema.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const profileSchema = new Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    age: { type: Number, required: true },
+});
+
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true, 
+    },
+    roles: {
+        type: [String],
+        default: ['user'], 
+    },
+    profile: {
+        type: profileSchema, 
+        required: true, 
+    },
+    lastLogin: {
+        type: Date, 
+        default: Date.now, 
+    },
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+
+module.exports = User;
